5ce43bdf308ca5e31d9bfbe585162bebcdb8eab6
Diogo <dramoslopesilveira@gmail.com>
2020-01-22 16:58:50 -0300

    enviando projeto sci

---
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/ControleInternoApplication.java
@@ -0,0 +1,13 @@
+package br.com.faespsenar.controleinterno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleInternoApplication.class, args);
+	}
+
+}
---
+++ controle-interno/src/test/java/br/com/faespsenar/controleinterno/ControleInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faespsenar.controleinterno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
