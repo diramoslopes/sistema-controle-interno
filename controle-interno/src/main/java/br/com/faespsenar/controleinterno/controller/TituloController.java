b2c410ea0c7d14ffb34575f9f0fdc9fa7eefc1a5
Diogo <dramoslopesilveira@gmail.com>
2020-01-23 12:29:07 -0300

    mostrando msg de sucesso com THYMELEAF

--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.faespsenar.controleinterno.model.Titulo;
 import br.com.faespsenar.controleinterno.repository.Titulos;
@@ -21,10 +22,13 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		return mv;
 	}
 	
 }
