a1a132e4d962570a03d65cf8b31c25228f05c837
Diogo <dramoslopesilveira@gmail.com>
2020-01-23 11:25:26 -0300

    2.3 enviando dados para o servidor

--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
@@ -2,12 +2,25 @@ package br.com.faespsenar.controleinterno.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.faespsenar.controleinterno.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		//salvar no banco de dados
+		
+		System.out.println(">>>>>>>> "  + titulo.getDescricao());
+		
 		return "CadastroTitulo";
 	}
 	
