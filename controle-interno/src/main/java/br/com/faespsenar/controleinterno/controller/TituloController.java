e8e6ed1ececcb519071fbf85de543d99d45b4d43
Diogo <dramoslopesilveira@gmail.com>
2020-01-23 16:45:05 -0300

    comboBox sendo montando dinamicamente com dados vindo do controller

--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
@@ -1,11 +1,16 @@
 package br.com.faespsenar.controleinterno.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.faespsenar.controleinterno.model.StatusTitulo;
 import br.com.faespsenar.controleinterno.model.Titulo;
 import br.com.faespsenar.controleinterno.repository.Titulos;
 
@@ -17,8 +22,11 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -28,7 +36,13 @@ public class TituloController {
 		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
 		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 	
 }
--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/model/StatusTitulo.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/model/StatusTitulo.java
@@ -3,7 +3,8 @@ package br.com.faespsenar.controleinterno.model;
 public enum StatusTitulo {
 	
 	PENDENTE("Pendente"),
-	RECEBIDO("Recebido");
+	RECEBIDO("Recebido"),
+	CANCELADO("Cancelado");
 	
 	private String descricao;
 	
