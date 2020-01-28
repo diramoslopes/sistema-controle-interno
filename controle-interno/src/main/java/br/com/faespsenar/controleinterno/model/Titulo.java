19f5ab3222047ecd0b64a94451a59915268e7692
Diogo <dramoslopesilveira@gmail.com>
2020-01-28 11:20:49 -0300

    validando o Bean titulo com bean validation ok

--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/model/Titulo.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/model/Titulo.java
@@ -11,6 +11,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -24,12 +28,18 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@javax.validation.constraints.NotEmpty(message = "Descrição é obrigatoria")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message = "Data de vencimento é obrigatoria")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@NotNull(message = "Valor não pode ser nulo")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999.99", message = "Valor não pode ser maior que 9.999.999,99")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
