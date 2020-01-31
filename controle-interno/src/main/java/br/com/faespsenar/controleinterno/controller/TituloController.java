da7eab2a47a9b0abf975b932f60cb3fecb4f922e
Diogo <dramoslopesilveira@gmail.com>
2020-01-31 10:13:25 -0300

    componente tooltip, datepicker, e mask de valores

--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -40,10 +41,14 @@ public class TituloController {
 		if(errors.hasErrors()) {
 			return CADASTRO_VIEW;
 		}
-		
-		titulos.save(titulo);
-		attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
+		try {
+			titulos.save(titulo);
+			attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
 			return "redirect:/titulos/novo";
+		} catch(DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, "Formato de data invalido");
+			return CADASTRO_VIEW;
+		}
 	}	
 	
 	@RequestMapping
--- controle-interno/src/main/java/br/com/faespsenar/controleinterno/model/Funcionario.java
+++ controle-interno/src/main/java/br/com/faespsenar/controleinterno/model/Funcionario.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import br.com.faespsenar.controleinterno.enumerator.TipoEntidade;
